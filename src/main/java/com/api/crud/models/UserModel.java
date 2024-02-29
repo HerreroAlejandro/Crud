872b259f26f30db93030713b296a1860cf5a32a6
HerreroAlejandro <160109043+HerreroAlejandro@users.noreply.github.com>
2024-02-29 05:19:00 -0300

    Validacion
    Se agrega anotaciones para validar los datos del usuario

--- src/main/java/com/api/crud/models/UserModel.java
+++ src/main/java/com/api/crud/models/UserModel.java
@@ -5,6 +5,11 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @Entity //Indica que cada campo que tenga la clase va a ser una columna en la bd
 @Table(name = "user")
 public class UserModel {
@@ -15,14 +20,19 @@ public class UserModel {
 
     @Column(name = "firstName")
     @Getter @Setter
+    @NotEmpty(message = "El campo nombre no puede ser vacio")
+    @Max(value = 50, message = "El nombre no puede tener mas de 50 caracteres")
     private String firstName;
 
     @Column(name = "lastName")
     @Getter @Setter
+    @NotEmpty(message = "El campo apellido no puede ser vacio")
+    @Max (value = 50, message = "El apellido no puede tener menos de 3 caracteres o mas de 50")
     private String lastName;
 
     @Column(name = "email")
     @Getter @Setter
+    @Email
     private String email;
 
 
